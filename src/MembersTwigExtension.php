0a6a52a9a87decff93d0e5fe1427de3e88900baa
Gawain Lynch <gawain.lynch@gmail.com>
2014-09-13 11:24:44 +0100

    Start of Twig functions
    Signed-off-by: Gawain Lynch <gawain.lynch@gmail.com>

---
+++ src/MembersTwigExtension.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members;
+
+/**
+ * Twig functions
+ */
+class MembersTwigExtension extends \Twig_Extension
+{
+    /**
+     * @var Application
+     */
+    private $app;
+
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * @var Bolt\Extension\Bolt\Members\Members
+     */
+    private $members;
+
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig = null;
+
+    public function __construct(\Silex\Application $app)
+    {
+        $this->app = $app;
+        $this->config = $this->app['extensions.' . Extension::NAME]->config;
+        $this->members = new Members($app);
+    }
+
+    public function initRuntime(\Twig_Environment $environment)
+    {
+        $this->twig = $environment;
+    }
+
+    /**
+     * Return the name of the extension
+     */
+    public function getName()
+    {
+        return 'members.twig';
+    }
+
+    /**
+     * The functions we add
+     */
+    public function getFunctions()
+    {
+        return array(
+            'members' => new \Twig_Function_Method($this, 'members'),
+        );
+    }
+
+    /**
+     * Return Twig suitable array for a member, or current session
+     *
+     * @param integer $id   [Optional] ID of member to look up
+     * @param boolean $meta [Optional] Return user meta
+     * @return \Twig_Markup
+     */
+    public function members($id = false, $meta = false)
+    {
+
+
+        return new \Twig_Markup($html, 'UTF-8');
+    }
+
+}
