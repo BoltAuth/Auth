d42a5359e97849153afd94c3f365d2892310b8ac
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-26 10:12:33 +0100

    Members extension controllers

---
+++ src/Controller/Frontend.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Controller;
+
+use Silex\Application;
+use Silex\ControllerCollection;
+use Silex\ControllerProviderInterface;
+
+/**
+ * Frontend controller.
+ *
+ * Copyright (C) 2014-2016 Gawain Lynch
+ *
+ * @author    Gawain Lynch <gawain.lynch@gmail.com>
+ * @copyright Copyright (c) 2014-2016, Gawain Lynch
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+class Frontend implements ControllerProviderInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function connect(Application $app)
+    {
+        /** @var $ctr ControllerCollection */
+        $ctr = $app['controllers_factory'];
+
+        return $ctr;
+    }
+}
