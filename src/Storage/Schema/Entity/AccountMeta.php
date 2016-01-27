59806f51a7b1e88e93fab83a129918edb218e8ec
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-27 15:14:30 +0100

    Account meta entity class

---
+++ src/Storage/Schema/Entity/AccountMeta.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Storage\Schema\Entity;
+
+use Bolt\Storage\Entity\Entity;
+
+/**
+ * Account meta entity class.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class AccountMeta extends Entity
+{
+    protected $guid;
+    protected $meta;
+    protected $value;
+
+    /**
+     * @return mixed
+     */
+    public function getGuid()
+    {
+        return $this->guid;
+    }
+
+    /**
+     * @param mixed $guid
+     */
+    public function setGuid($guid)
+    {
+        $this->guid = $guid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMeta()
+    {
+        return $this->meta;
+    }
+
+    /**
+     * @param mixed $meta
+     */
+    public function setMeta($meta)
+    {
+        $this->meta = $meta;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+}
