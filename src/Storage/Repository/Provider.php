09da3014f8bdc2308eebf58d556b3f3e27c186af
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-27 21:19:04 +0100

    Queries for Provider repository

--- src/Storage/Repository/Provider.php
+++ src/Storage/Repository/Provider.php
@@ -2,6 +2,7 @@
 
 namespace Bolt\Extension\Bolt\Members\Storage\Repository;
 
+use Bolt\Extension\Bolt\Members\Storage\Entity;
 use Bolt\Storage\Repository;
 
 /**
@@ -11,6 +12,106 @@ use Bolt\Storage\Repository;
  */
 class Provider extends Repository
 {
+    /**
+     * Fetches Provider entries by GUID.
+     *
+     * @param string $guid
+     *
+     * @return Entity\Provider[]
+     */
+    public function getProvisionsByGuid($guid)
+    {
+        $query = $this->getProvisionsByGuidQuery($guid);
+
+        return $this->findWith($query);
+    }
+
+    public function getProvisionsByGuidQuery($guid)
+    {
+        $qb = $this->createQueryBuilder();
+        $qb->select('*')
+            ->where('guid = :guid')
+            ->setParameter('guid', $guid)
+        ;
+
+        return $qb;
+    }
+
+    /**
+     * Fetches Provider entries by provider name.
+     *
+     * @param string $provider
+     *
+     * @return Entity\Provider[]
+     */
+    public function getProvisionsByProvider($provider)
+    {
+        $query = $this->getProvisionsByProviderQuery($provider);
+
+        return $this->findWith($query);
+    }
+
+    public function getProvisionsByProviderQuery($provider)
+    {
+        $qb = $this->createQueryBuilder();
+        $qb->select('*')
+            ->where('provider = :provider')
+            ->setParameter('provider', $provider)
+        ;
+
+        return $qb;
+    }
+
+    /**
+     * Fetches Provider entry by resource owner.
+     *
+     * @param string $resourceOwner
+     *
+     * @return Entity\Provider
+     */
+    public function getProvisionByResourceOwner($resourceOwner)
+    {
+        $query = $this->getProvisionByResourceOwnerQuery($resourceOwner);
+
+        return $this->findWith($query);
+    }
+
+    public function getProvisionByResourceOwnerQuery($resourceOwner)
+    {
+        $qb = $this->createQueryBuilder();
+        $qb->select('*')
+            ->where('resource_owner = :resource_owner')
+            ->setParameter('resource_owner', $resourceOwner)
+        ;
+
+        return $qb;
+    }
+
+    /**
+     * Fetches an Provider entries by resource owner ID.
+     *
+     * @param string $resourceOwnerId
+     *
+     * @return Entity\Provider
+     */
+    public function getProvisionByResourceOwnerId($resourceOwnerId)
+    {
+        $query = $this->getProvisionByResourceOwnerIdQuery($resourceOwnerId);
+
+        return $this->findWith($query);
+    }
+
+    public function getProvisionByResourceOwnerIdQuery($resourceOwnerId)
+    {
+        $qb = $this->createQueryBuilder();
+        $qb->select('*')
+            ->where('resource_owner_id = :resource_owner_id')
+            ->setParameter('resource_owner_id', $resourceOwnerId)
+        ;
+
+        return $qb;
+    }
+
     /**
      * Creates a query builder instance namespaced to this repository
      *
