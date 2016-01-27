7c146c37248c0b2c88676c0e58e14ff5a4df94f9
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-27 15:14:19 +0100

    Account entity class

---
+++ src/Storage/Schema/Entity/Account.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Storage\Schema\Entity;
+
+use Bolt\Storage\Entity\Entity;
+
+/**
+ * Account entity class.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Account extends Entity
+{
+    protected $guid;
+    protected $username;
+    protected $email;
+    protected $lastseen;
+    protected $lastip;
+    protected $displayname;
+    protected $enabled;
+    protected $roles;
+
+    /**
+     * @return mixed
+     */
+    public function getGuid()
+    {
+        return $this->guid;
+    }
+
+    /**
+     * @param mixed $guid
+     */
+    public function setGuid($guid)
+    {
+        $this->guid = $guid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastseen()
+    {
+        return $this->lastseen;
+    }
+
+    /**
+     * @param mixed $lastseen
+     */
+    public function setLastseen($lastseen)
+    {
+        $this->lastseen = $lastseen;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastip()
+    {
+        return $this->lastip;
+    }
+
+    /**
+     * @param mixed $lastip
+     */
+    public function setLastip($lastip)
+    {
+        $this->lastip = $lastip;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDisplayname()
+    {
+        return $this->displayname;
+    }
+
+    /**
+     * @param mixed $displayname
+     */
+    public function setDisplayname($displayname)
+    {
+        $this->displayname = $displayname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param mixed $enabled
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param mixed $roles
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+    }
+}
