8739aaafbbf210a2c2162d440dc62194d65ba94d
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-27 14:49:40 +0100

    Account meta table

---
+++ src/Storage/Schema/Table/AccountMeta.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Storage\Schema\Table;
+
+use Bolt\Storage\Database\Schema\Table\BaseTable;
+
+/**
+ * Account meta table.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class AccountMeta extends BaseTable
+{
+    /**
+     * @inheritDoc
+     */
+    protected function addColumns()
+    {
+        $this->table->addColumn('guid',   'guid',     []);
+        $this->table->addColumn('meta',   'string',  ['length' => 64]);
+        $this->table->addColumn('value',  'text');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function addIndexes()
+    {
+        $this->table->addIndex(['meta']);
+
+        //$this->table->addForeignKeyConstraint($this->tableName, ['guid'], ['guid']);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function setPrimaryKey()
+    {
+        $this->table->setPrimaryKey(['guid']);
+    }
+
+}
