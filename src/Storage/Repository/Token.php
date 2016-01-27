69cff3a2e4fb15bd00d90cb348e2220a0068ea56
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-27 21:37:07 +0100

    Queries for Token repository

--- src/Storage/Repository/Token.php
+++ src/Storage/Repository/Token.php
@@ -2,7 +2,10 @@
 
 namespace Bolt\Extension\Bolt\Members\Storage\Repository;
 
+use Bolt\Extension\Bolt\Members\Storage\Entity;
+use Bolt\Storage\Mapping\Type\CarbonDateTimeType;
 use Bolt\Storage\Repository;
+use Carbon\Carbon;
 
 /**
  * Token repository.
@@ -12,11 +15,85 @@ use Bolt\Storage\Repository;
 class Token extends Repository
 {
     /**
+     * Fetches Token entries by GUID.
+     *
+     * @param string $guid
+     *
+     * @return Entity\Token[]
+     */
+    public function getTokensByGuid($guid)
+    {
+        $query = $this->getTokensByGuidQuery($guid);
+
+        return $this->findWith($query);
+    }
+
+    public function getTokensByGuidQuery($guid)
+    {
+        $qb = $this->createQueryBuilder();
+        $qb->select('*')
+            ->where('guid = :guid')
+            ->setParameter('guid', $guid)
+        ;
+
+        return $qb;
+    }
+
+    /**
+     * Fetches all tokens by cookie.
+     *
+     * @param $cookie
+     *
+     * @return Entity\Token[]
+     */
+    public function getTokensByCookie($cookie)
+    {
+        $query = $this->getTokensByCookieQuery($cookie);
+
+        return $this->findWith($query);
+    }
+
+    public function getTokensByCookieQuery($cookie)
+    {
+        $qb = $this->createQueryBuilder();
+        $qb->select('*')
+            ->where('cookie = :cookie')
+            ->setParameter('cookie', $cookie)
+        ;
+
+
+        return $qb;
+    }
+
+    /**
+     * Fetches expired tokens.
+     *
+     * @return Entity\Account
+     */
+    public function getTokensExpired()
+    {
+        $query = $this->getTokensExpiredQuery();
+
+        return $this->findOneWith($query);
+    }
+
+    public function getTokensExpiredQuery()
+    {
+        $qb = $this->createQueryBuilder();
+        $qb->select('*')
+            ->where('expired < :now')
+            ->setParameter('now', Carbon::now(), CarbonDateTimeType::DATETIME)
+        ;
+
+        return $qb;
+    }
+
+    /**
      * Creates a query builder instance namespaced to this repository
      *
      * @return \Doctrine\DBAL\Query\QueryBuilder
      */
-    public function createQueryBuilder($alias = null)
+    public function createQueryBuilder()
     {
         return $this->em->createQueryBuilder()
             ->from($this->getTableName());
