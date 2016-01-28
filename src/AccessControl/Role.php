11b65134fefa7940a4ee3bcfef6b5e707d1157ae
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-28 11:40:17 +0100

    Single role class

---
+++ src/AccessControl/Role.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\AccessControl;
+
+/**
+ * Single role class.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Role
+{
+    /** @var string */
+    protected $name;
+    /** @var string */
+    protected $displayName;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Role
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDisplayName()
+    {
+        return $this->displayName;
+    }
+
+    /**
+     * @param string $displayName
+     *
+     * @return Role
+     */
+    public function setDisplayName($displayName)
+    {
+        $this->displayName = $displayName;
+
+        return $this;
+    }
+}
