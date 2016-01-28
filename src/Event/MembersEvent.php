e098f1ffd667759c753a8e98602d929060982eff
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-28 11:36:24 +0100

    Event properties

--- src/Event/MembersEvent.php
+++ src/Event/MembersEvent.php
@@ -2,6 +2,7 @@
 
 namespace Bolt\Extension\Bolt\Members\Event;
 
+use Bolt\Extension\Bolt\Members\Storage\Entity;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
 /**
@@ -11,4 +12,48 @@ use Symfony\Component\EventDispatcher\GenericEvent;
  */
 class MembersEvent extends GenericEvent
 {
+    /** @var Entity\Account */
+    protected $account;
+    /** @var array */
+    protected $roles;
+
+    /**
+     * @return Entity\Account
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
+
+    /**
+     * @param Entity\Account $account
+     *
+     * @return MembersEvent
+     */
+    public function setAccount(Entity\Account $account)
+    {
+        $this->account = $account;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param array $roles
+     *
+     * @return MembersEvent
+     */
+    public function setRoles(array $roles)
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
 }
