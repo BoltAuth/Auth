ce086f470de91f57aa693e8498c082c5895b9e61
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-28 11:24:42 +0100

    Members event class

---
+++ src/Event/MembersEvent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Event;
+
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+/**
+ * Members event class.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class MembersEvent extends GenericEvent
+{
+}
