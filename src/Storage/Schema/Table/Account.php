6353cfa18cff4ab046255c56d2064e3793994905
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-29 11:34:24 +0100

    Drop username as it will be used/available in meta

--- src/Storage/Schema/Table/Account.php
+++ src/Storage/Schema/Table/Account.php
@@ -17,13 +17,12 @@ class Account extends BaseTable
     protected function addColumns()
     {
         $this->table->addColumn('guid',        'guid',       []);
-        $this->table->addColumn('username',    'string',     ['length' => 32]);
-        $this->table->addColumn('email',       'string',     ['length' => 128]);
-        $this->table->addColumn('lastseen',    'datetime',   ['default' => '1900-01-01 00:00:00']);
-        $this->table->addColumn('lastip',      'string',     ['length' => 32, 'notnull' => false, 'default' => '']);
-        $this->table->addColumn('displayname', 'string',     ['length' => 32, 'notnull' => false]);
+        $this->table->addColumn('email',       'string',     ['length'  => 128]);
+        $this->table->addColumn('displayname', 'string',     ['length'  => 32, 'notnull' => false]);
         $this->table->addColumn('enabled',     'boolean',    ['default' => 0]);
-        $this->table->addColumn('roles',       'json_array', ['length' => 1024, 'default' => '']);
+        $this->table->addColumn('roles',       'json_array', ['length'  => 1024, 'notnull' => false]);
+        $this->table->addColumn('lastseen',    'datetime',   ['default' => '']);
+        $this->table->addColumn('lastip',      'string',     ['length'  => 32, 'notnull' => false]);
     }
 
     /**
@@ -31,7 +30,6 @@ class Account extends BaseTable
      */
     protected function addIndexes()
     {
-        $this->table->addUniqueIndex(['username']);
         $this->table->addUniqueIndex(['email']);
 
         $this->table->addIndex(['enabled']);
