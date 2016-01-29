c46bc03ed3d9c0252082acf57bcd0ccb17836964
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-29 17:25:18 +0100

    Register form object entity

---
+++ src/Form/Entity/Register.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Form\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * User registration object class
+ *
+ * Copyright (C) 2014-2016 Gawain Lynch
+ *
+ * @author    Gawain Lynch <gawain.lynch@gmail.com>
+ * @copyright Copyright (c) 2014-2016, Gawain Lynch
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+class Register
+{
+    /** string */
+    protected $email;
+    /** string */
+    protected $displayName;
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     *
+     * @return Register
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDisplayName()
+    {
+        return $this->displayName;
+    }
+
+    /**
+     * @param mixed $displayName
+     *
+     * @return Register
+     */
+    public function setDisplayName($displayName)
+    {
+        $this->displayName = $displayName;
+
+        return $this;
+    }
+}
