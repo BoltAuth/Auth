dd9829cd3d97bb226110f1d3cfb1b16896df0ea7
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-30 12:04:14 +0100

    Temporarily fake an access token for Local providers

--- src/Oauth2/Client/Provider/Local.php
+++ src/Oauth2/Client/Provider/Local.php
@@ -4,6 +4,7 @@ namespace Bolt\Extension\Bolt\Members\Oauth2\Client\Provider;
 
 use League\OAuth2\Client\Provider\GenericProvider as LeagueGenericProvider;
 use League\OAuth2\Client\Token\AccessToken;
+use Ramsey\Uuid\Uuid;
 
 /**
  * Local provider extension.
@@ -12,6 +13,31 @@ use League\OAuth2\Client\Token\AccessToken;
  */
 class Local extends LeagueGenericProvider
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function getRequiredOptions()
+    {
+        // Temporarily fake an access token for Local provider
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAccessToken($grant, array $options = [])
+    {
+        // Temporarily fake an access token for Local provider.
+        $defaultOptions = [
+            'access_token'      => Uuid::uuid4()->toString(),
+            'resource_owner_id' => Uuid::uuid4()->toString(),
+            'refresh_token'     => Uuid::uuid4()->toString(),
+            'expires'           => 86400,
+        ];
+
+        return new AccessToken(array_merge($defaultOptions, $options));
+    }
+
     /**
      * {@inheritdoc}
      */
