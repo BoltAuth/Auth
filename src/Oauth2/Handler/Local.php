4093ae064c05b1cd96db10c8d2aafafbda8127a3
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-01 11:42:06 +0100

    Initial OAuth local login provider class

---
+++ src/Oauth2/Handler/Local.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Oauth2\Handler;
+
+/**
+ * OAuth local login provider.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Local extends HandlerBase
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function login()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process($grantType = 'authorization_code')
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function logout()
+    {
+    }
+}
