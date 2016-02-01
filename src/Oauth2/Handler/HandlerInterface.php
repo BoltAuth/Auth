d3a5679968563606606fe64fb6a1764e57452f5a
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-01 11:35:42 +0100

    Require request for the handler interface

--- src/Oauth2/Handler/HandlerInterface.php
+++ src/Oauth2/Handler/HandlerInterface.php
@@ -4,6 +4,7 @@ namespace Bolt\Extension\Bolt\Members\Oauth2\Handler;
 
 use Bolt\Extension\Bolt\Members\Exception\DisabledProviderException;
 use Bolt\Extension\Bolt\Members\Exception\InvalidAuthorisationRequestException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -16,26 +17,31 @@ interface HandlerInterface
     /**
      * Login a client.
      *
+     * @param Request $request
+     *
      * @throws DisabledProviderException
      * @throws InvalidAuthorisationRequestException
      *
      * @return Response
      */
-    public function login();
+    public function login(Request $request);
 
     /**
      * Process a OAuth2 provider login callback.
      *
-     * @param string $grantType
+     * @param Request $request
+     * @param string  $grantType
      *
      * @return Response
      */
-    public function process($grantType);
+    public function process(Request $request, $grantType);
 
     /**
      * Logout a client.
      *
+     * @param Request $request
+     *
      * @return Response
      */
-    public function logout();
+    public function logout(Request $request);
 }
