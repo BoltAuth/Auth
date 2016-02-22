a4f265224e28af50570c5e020de34c8bc636b106
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-22 14:06:55 +0100

    Additional exception constants

--- src/Exception/InvalidProviderException.php
+++ src/Exception/InvalidProviderException.php
@@ -5,4 +5,6 @@ namespace Bolt\Extension\Bolt\Members\Exception;
 class InvalidProviderException extends \Exception
 {
     const INVALID_PROVIDER = 'Invalid provider.';
+    const UNSET_PROVIDER = 'Unset provider.';
+    const UNMAPPED_PROVIDER = 'Unmapped provider.';
 }
