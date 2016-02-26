9608b6d550fbc0ce241399ae9292731b89817aa6
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-26 10:11:05 +0100

    Password reset form entity

---
+++ src/Form/Entity/ProfilePasswordReset.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Form\Entity;
+
+/**
+ * User profile password reset object.
+ *
+ * Copyright (C) 2014-2016 Gawain Lynch
+ *
+ * @author    Gawain Lynch <gawain.lynch@gmail.com>
+ * @copyright Copyright (c) 2014-2016, Gawain Lynch
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+class ProfilePasswordReset implements EntityInterface
+{
+}
