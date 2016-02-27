11c6b6ba037ace780dfd6cc3a2e4238083eb4c05
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-27 11:34:58 +0100

    Members notification failure event class

---
+++ src/Event/MembersNotificationFailureEvent.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Event;
+
+use Swift_Mime_Message as SwiftMimeMessage;
+use Swift_RfcComplianceException as SwiftRfcComplianceException;
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Members notification failure event class.
+ *
+ * Copyright (C) 2014-2016 Gawain Lynch
+ *
+ * @author    Gawain Lynch <gawain.lynch@gmail.com>
+ * @copyright Copyright (c) 2014-2016, Gawain Lynch
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+class MembersNotificationFailureEvent extends Event
+{
+    /** @var SwiftMimeMessage */
+    protected $message;
+    /** @var SwiftRfcComplianceException */
+    protected $exception;
+
+    /**
+     * Constructor.
+     *
+     * @param SwiftMimeMessage            $message
+     * @param SwiftRfcComplianceException $exception
+     */
+    public function __construct(SwiftMimeMessage $message, SwiftRfcComplianceException $exception)
+    {
+        $this->message = $message;
+        $this->exception = $exception;
+    }
+
+    /**
+     * @return SwiftMimeMessage
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @return SwiftRfcComplianceException
+     */
+    public function getException()
+    {
+        return $this->exception;
+    }
+}
