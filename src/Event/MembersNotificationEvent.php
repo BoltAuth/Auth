29af368b8835bbe6a376f327adf445453aece6ec
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-27 11:32:29 +0100

    Notification event class

---
+++ src/Event/MembersNotificationEvent.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Event;
+
+use Swift_Mime_Message as SwiftMimeMessage;
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Members notification event class.
+ *
+ * Copyright (C) 2014-2016 Gawain Lynch
+ *
+ * @author    Gawain Lynch <gawain.lynch@gmail.com>
+ * @copyright Copyright (c) 2014-2016, Gawain Lynch
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+class MembersNotificationEvent extends Event
+{
+    /** @var SwiftMimeMessage */
+    protected $message;
+
+    /**
+     * Constructor.
+     *
+     * @param SwiftMimeMessage $message
+     */
+    public function __construct(SwiftMimeMessage $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return SwiftMimeMessage
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
