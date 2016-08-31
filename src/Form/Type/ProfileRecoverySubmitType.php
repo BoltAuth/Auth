b488b351bb1e70688648682d3300b5c57d1e2a1d
Gawain Lynch <gawain.lynch@gmail.com>
2016-08-31 14:58:53 +0200

    Split out password recovery request & submit

---
+++ src/Form/Type/ProfileRecoverySubmitType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Form\Type;
+
+/**
+ * Password reset submission type.
+ *
+ * Copyright (C) 2014-2016 Gawain Lynch
+ *
+ * @author    Gawain Lynch <gawain.lynch@gmail.com>
+ * @copyright Copyright (c) 2014-2016, Gawain Lynch
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+class ProfileRecoverySubmitType extends AbstractProfileRecoveryType
+{
+    /** @var boolean */
+    protected $requirePassword = true;
+}
