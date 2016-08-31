e19d2c7e8cbec64d36a38bc64e61658d6281044e
Gawain Lynch <gawain.lynch@gmail.com>
2016-08-31 12:30:57 +0200

    Interface for classes needing session

---
+++ src/Form/SessionAwareInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Form;
+
+use Bolt\Extension\Bolt\Members\AccessControl\Session;
+
+/**
+ * Interface for classes needing access control session.
+ *
+ * Copyright (C) 2014-2016 Gawain Lynch
+ *
+ * @author    Gawain Lynch <gawain.lynch@gmail.com>
+ * @copyright Copyright (c) 2014-2016, Gawain Lynch
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+interface SessionAwareInterface
+{
+    /**
+     * @param Session $session
+     */
+    public function setSession(Session $session);
+}
