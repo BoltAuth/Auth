4b641836df40271a931ef9c58b740439158968d4
Gawain Lynch <gawain.lynch@gmail.com>
2016-08-31 10:36:51 +0200

    Separate profile view & edit types

---
+++ src/Form/Type/ProfileViewType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Members\Form\Type;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Profile view type.
+ *
+ * Copyright (C) 2014-2016 Gawain Lynch
+ *
+ * @author    Gawain Lynch <gawain.lynch@gmail.com>
+ * @copyright Copyright (c) 2014-2016, Gawain Lynch
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+class ProfileViewType extends ProfileEditType
+{
+    /** @var boolean */
+    protected $requirePassword = false;
+}
