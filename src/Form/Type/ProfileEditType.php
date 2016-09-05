23c338979c240edd16af31e651f779f40b02815d
Gawain Lynch <gawain.lynch@gmail.com>
2016-09-05 17:56:05 +0200

    More centralisation of profile types

--- src/Form/Type/ProfileEditType.php
+++ src/Form/Type/ProfileEditType.php
@@ -3,11 +3,7 @@
 namespace Bolt\Extension\Bolt\Members\Form\Type;
 
 use Bolt\Translation\Translator as Trans;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -20,7 +16,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @copyright Copyright (c) 2014-2016, Gawain Lynch
  * @license   https://opensource.org/licenses/MIT MIT
  */
-class ProfileEditType extends AbstractType
+class ProfileEditType extends AbstractProfileType
 {
     /** @var boolean */
     protected $requirePassword = true;
@@ -31,57 +27,6 @@ class ProfileEditType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add(
-                'displayname',
-                TextType::class,
-                [
-                    'label'       => Trans::__($this->config->getLabel('displayname')),
-                    'data'        => $this->getData($options, 'displayname'),
-                    'attr'        => [
-                        'placeholder' => $this->config->getPlaceholder('displayname'),
-                    ],
-                    'constraints' => [
-                        new Assert\NotBlank(),
-                        new Assert\Length(['min' => 2]),
-                    ],
-                ]
-            )
-            ->add(
-                'email',
-                EmailType::class,
-                [
-                    'label'       => Trans::__($this->config->getLabel('email')),
-                    'data'        => $this->getData($options, 'email'),
-                    'attr'        => [
-                        'placeholder' => $this->config->getPlaceholder('email'),
-                    ],
-                    'constraints' => new Assert\Email([
-                        'message' => 'The address "{{ value }}" is not a valid email.',
-                        'checkMX' => true,
-                    ]),
-                ]
-            )
-            ->add(
-                'password',
-                RepeatedType::class,
-                [
-                    'type'           => PasswordType::class,
-                    'first_options'  => [
-                        'label'       => Trans::__($this->config->getLabel('password_first')),
-                        'attr'        => [
-                            'placeholder' => $this->config->getPlaceholder('password_first'),
-                        ],
-                    ],
-                    'second_options' => [
-                        'label'       => Trans::__($this->config->getLabel('password_second')),
-                        'attr'        => [
-                            'placeholder' => $this->config->getPlaceholder('password_second'),
-                        ],
-                    ],
-                    'empty_data'     => null,
-                    'required'       => $this->requirePassword,
-                ]
-            )
             ->add(
                 'submit',
                 SubmitType::class,
