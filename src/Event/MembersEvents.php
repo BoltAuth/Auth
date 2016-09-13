171a47353a66d972efffb0f471c66f825ddfaa6c
Gawain Lynch <gawain.lynch@gmail.com>
2016-09-13 11:58:02 +0200

    Add a profile resetevent constant

--- src/Event/MembersEvents.php
+++ src/Event/MembersEvents.php
@@ -21,6 +21,7 @@ class MembersEvents
     const MEMBER_PROFILE_PRE_SAVE = 'member.profile.pre_save';
     const MEMBER_PROFILE_POST_SAVE = 'member.profile.post_save';
     const MEMBER_PROFILE_REGISTER = 'member.profile.register';
+    const MEMBER_PROFILE_RESET = 'member.profile.reset';
     const MEMBER_PROFILE_VERIFY = 'member.profile.verify';
     const MEMBER_ROLE = 'member.role';
     const MEMBER_NOTIFICATION_PRE_SEND = 'member.notification.pre_send';
